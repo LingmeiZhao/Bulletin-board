d45dff8f334b79d7e2c32a7b9d77da0ba2e00eb9
LingmeiZhao <ZhaoLingmei0@outlook.com>
2018-11-14 15:12:15 -0500

    Updates

---
+++ precache-manifest.681f2e118897d1796bc39ff617c60185.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e69a572c86ed0b1adec",
+    "url": "/bulletin-board/static/css/main.b78091e3.chunk.css"
+  },
+  {
+    "revision": "3e69a572c86ed0b1adec",
+    "url": "/bulletin-board/static/js/main.3e69a572.chunk.js"
+  },
+  {
+    "revision": "41cb2b0981fdfc73f0b8",
+    "url": "/bulletin-board/static/js/1.41cb2b09.chunk.js"
+  },
+  {
+    "revision": "815a3d4b338105962b38",
+    "url": "/bulletin-board/static/js/runtime~main.815a3d4b.js"
+  },
+  {
+    "revision": "1e2671e6ed93c343cf1b6f9d3acd29a5",
+    "url": "/bulletin-board/index.html"
+  }
+];
